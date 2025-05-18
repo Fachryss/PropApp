5f39e120e3185921aec157bff82a428a294e7d79
lordbruh0 <ryohariyono17@gmail.com>
2025-05-18 15:14:30 +0700

    perbaikan intro

--- lib/screen/intro_page.dart
+++ lib/screen/intro_page.dart
@@ -2,6 +2,17 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
 
+// Class untuk mengelola gambar dengan waktu tampil
+class TimedImage {
+  final int timeInSeconds;
+  final String imagePath;
+
+  TimedImage({
+    required this.timeInSeconds,
+    required this.imagePath,
+  });
+}
+
 class IntroPage extends StatefulWidget {
   const IntroPage({super.key});
 
@@ -13,27 +24,66 @@ class _IntroPageState extends State<IntroPage> {
   final AudioPlayer _audioPlayer = AudioPlayer();
   int _currentImageIndex = 0;
   late Timer _timer;
+  int _elapsedTimeInSeconds = 0;
 
-  final List<String> _imagePaths = List.generate(
-    8,
-    (index) => 'assets/images/gambar_${index + 1}.jpg',
-  );
+  // Daftar gambar dengan waktu tayang masing-masing
+  final List<TimedImage> _timedImages = [
+    TimedImage(
+        timeInSeconds: 0, imagePath: 'assets/images/gambar_9.jpg'), // Intro
+    TimedImage(
+        timeInSeconds: 7, imagePath: 'assets/images/gambar_1.jpg'), // Skincare
+    TimedImage(
+        timeInSeconds: 16,
+        imagePath: 'assets/images/gambar_2.jpg'), // serum wajah
+    TimedImage(
+        timeInSeconds: 21, imagePath: 'assets/images/gambar_6.jpg'), // tanya
+    TimedImage(
+        timeInSeconds: 34,
+        imagePath: 'assets/images/gambar_4.jpg'), // giveaway Instagram
+    TimedImage(
+        timeInSeconds: 37,
+        imagePath: 'assets/images/gambar_3.jpg'), // diskon 20%
+    TimedImage(
+        timeInSeconds: 41,
+        imagePath: 'assets/images/gambar_5.jpg'), // kolaborasi influencer
+    TimedImage(
+        timeInSeconds: 45, imagePath: 'assets/images/gambar_10.jpg'), // peluang
+    TimedImage(timeInSeconds: 89, imagePath: 'assets/images/gambar_3.jpg'),
+    TimedImage(timeInSeconds: 91, imagePath: 'assets/images/gambar_5.jpg'),
+    TimedImage(timeInSeconds: 97, imagePath: 'assets/images/gambar_7.jpg'),
+    TimedImage(timeInSeconds: 105, imagePath: 'assets/images/gambar_8.jpg'),
+  ];
 
   @override
   void initState() {
     super.initState();
     _playAudio();
-    _startImageSlider();
+    _startTimedImageDisplay();
   }
 
   Future<void> _playAudio() async {
     await _audioPlayer.play(AssetSource('audio/intro.mp3'));
   }
 
-  void _startImageSlider() {
-    _timer = Timer.periodic(const Duration(seconds: 2), (timer) {
+  void _startTimedImageDisplay() {
+    // Buat timer yang menjalankan pengecekan setiap detik
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
       setState(() {
-        _currentImageIndex = (_currentImageIndex + 1) % _imagePaths.length;
+        // Tambah hitungan waktu
+        _elapsedTimeInSeconds++;
+
+        // Cek apakah perlu mengganti gambar
+        for (int i = 0; i < _timedImages.length; i++) {
+          // Cari gambar yang seharusnya ditampilkan pada waktu saat ini
+          if (_elapsedTimeInSeconds >= _timedImages[i].timeInSeconds &&
+              (i == _timedImages.length - 1 ||
+                  _elapsedTimeInSeconds < _timedImages[i + 1].timeInSeconds)) {
+            if (_currentImageIndex != i) {
+              _currentImageIndex = i;
+              break;
+            }
+          }
+        }
       });
     });
   }
@@ -80,11 +130,11 @@ class _IntroPageState extends State<IntroPage> {
                           ),
                         ),
                         const SizedBox(height: 20),
-                        // Kotak gambar slideshow
+                        // Kotak gambar dengan sistem waktu
                         ClipRRect(
                           borderRadius: BorderRadius.circular(16),
                           child: Image.asset(
-                            _imagePaths[_currentImageIndex],
+                            _timedImages[_currentImageIndex].imagePath,
                             width: double.infinity,
                             height: 200,
                             fit: BoxFit.cover,
